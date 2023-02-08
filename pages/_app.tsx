920b92359de3ad2c7c2a981049816a3422402203
mrpaulishaili <paulishaili@gmail.com>
2023-02-08 03:52:53 +0100

    ,...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import routerProvider from '@pankod/refine-nextjs-router';
 import dataProvider from '@pankod/refine-simple-rest';
 
 import '/styles/global.css';
+
 import { DefaultLayout } from 'layouts/DefaultLayout';
 
 const API_URL = 'https://fakestoreapi.com';
