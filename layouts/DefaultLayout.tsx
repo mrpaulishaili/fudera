c95d482c78ee7bf786c9448bace646a200670a26
mrpaulishaili <paulishaili@gmail.com>
2023-02-08 03:56:19 +0100

    ...

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import { LayoutProps } from '@pankod/refine-core';
 
 export const DefaultLayout: React.FC<LayoutProps> = ({ children }) => {
-  return children;
+  return <>{children}</>;
 };
